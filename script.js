e7cfa74b8f450d6b25b6cc02b01467e836582c09
unknown <santosfrank2010@gmail.com>
2022-09-09 17:49:14 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// *getting data from an external api
+
+// *use the fetch function
+
+function search() {
+  var value = document.getElementById("searchTerm").value;
+  fetch(
+    `https://pixabay.com/api/?key=19926008-b9e8fc0039d8af8f8061550dc&q=${value}`
+  )
+    .then((reponse) => {
+      return reponse.json();
+    })
+    .then((data) => {
+      let result = data.hits; //store the collected hist in your result var
+      console.log(result); //test
+      //  *print data on the screen
+      document.getElementById("container").innerHTML = `
+        ${result
+          .map((pics) => {
+            return `
+            <div class="card">
+            <a href="${pics.webformatURL}" class="glightbox">
+              <img src="${pics.webformatURL}">
+            </a>
+            
+              <div class="userDetails">
+                     <h4>${pics.user}</h4>
+                    <div class="profile">
+                    <img src="${
+                      !pics.userImageURL
+                        ? "https://dl.memuplay.com/new_market/img/com.vicman.newprofilepic.icon.2022-06-07-21-33-07.png"
+                        : pics.userImageURL
+                    }">
+                    </div>
+              </div>
+            </div>
+            `;
+          })
+          .join(" ")}
+    
+    `;
+
+           if (result.length == 0) {
+             document.getElementById(
+               "container"
+             ).innerHTML = ` <h1>No Result Found for "${value}"</h1> `;
+           }
+    });
+
+   
+}
+search();
